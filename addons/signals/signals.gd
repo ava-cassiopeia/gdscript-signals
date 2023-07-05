aeee110ee39d76b80e5736193374ae5d63984c9e
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2023-07-05 09:20:53 -0600

    First attempt at implementing Signals.all()

--- addons/signals/signals.gd
+++ addons/signals/signals.gd
@@ -5,6 +5,12 @@ class_name Signals extends EditorPlugin
 
 
 # Given a list of signals, waits until each signal has been recieved at least
-# once, then returns.
-static func all(signal_list: Array[Signal]):
-	pass
+# once, then returns all of the signals' return values.
+static func all(signal_list: Array[Signal])->Array[Object]:
+	var output: Array[Object] = []
+	# Use 'single_signal' instead of 'signal' here because 'signal' is a
+	# keyword.
+	for single_signal in signal_list:
+		var result = await single_signal
+		output.append(result)
+	return output
